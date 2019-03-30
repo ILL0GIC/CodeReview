6e46d177d8f91fdcf1eb98eaee57ffa9fd8c63a5
Jon Michael Ossola <jonmichaelossola@Jons-MacBook-Pro.local>
2019-03-30 11:11:00 -0600

    added navbar component

---
+++ code-review-app/src/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const NavBar = () => {
+    return (
+        <div className='navbarstylinghere'>
+            
+        </div>
+    );
+}
+
+export default NavBar;
