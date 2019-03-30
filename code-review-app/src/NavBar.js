c7c775afb1e49a71532a122933ec3716d15a3c0a
Heather <heather@Heathers-MacBook-Air.local>
2019-03-30 14:51:06 -0600

    made it pretty

--- code-review-app/src/NavBar.js
+++ code-review-app/src/NavBar.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <div className='navbarstylinghere'>
-            
-        </div>
+        <div className='navbarstylinghere background'>
+            <ul>
+            	<li className="title">Group Five</li>
+            	<li className="input button"><a href="" target="_blank">Login</a></li>
+            	<li className="input button"><a href="" target="_blank">Sign Up</a></li>
+            </ul>
+		</div>
     );
 }
 
